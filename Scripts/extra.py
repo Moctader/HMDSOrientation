2997bdb2082da05d6e307146742c4597262bda7d
Moctader <md.moctader@student.oulu.fi>
2019-11-12 14:09:26 +0200

    PLM Loaad done

--- Scripts/extra.py
+++ Scripts/extra.py
@@ -12,42 +12,54 @@ import matplotlib.pyplot as plt
 from skimage.io import imsave
 import time
 from Components.Utilities import load, print_orthogonal
+from scipy.io import loadmat
+import os
+
+
+def PLM_plot(images):
+    plt.imshow(images['az90'])
+    plt.show()
+    names = ['az90', 'az', 'ret']
+    values = [1, 3, 27]
+    plt.figure(figsize=(27, 3))
+
 
 
 if __name__ == '__main__':
 
-    path = "/data/Repositories/HMDS_orientation/Data/"
+    HMDS_path = "/data/Repositories/HMDS_orientation/Data/"
+    PLM_path = '/data/Repositories/HMDS_orientation/Stitched_PLI/'
     n_jobs = 12
 
 
-    # Alternate way to load
-    files = []
-    for i in os.listdir(path):
-        data = load(path+i, n_jobs=n_jobs)
-
-        print_orthogonal(data)
-        print(data.shape)
 
 
-    print(files)
+
+
 
 
     # TODO: Load the PLM images
 
-    from scipy.io import loadmat
-    import os
 
-    file_list = os.listdir('/data/Repositories/HMDS_orientation/Stitched_PLI/')
+
+    file_list = os.listdir(PLM_path)
     print(file_list)
     for filename in sorted(file_list):
         if filename.endswith('.mat'):
             print(filename)
-            annots = loadmat(filename)
-            print(annots)
+            annots = loadmat(PLM_path+filename)
+            PLM_plot(annots)
 
 
 
 
 
+    # Load HMDS
+    files = []
+    for i in os.listdir(HMDS_path):
+        data = load(HMDS_path + i, n_jobs=n_jobs)
 
+        print_orthogonal(data)
+        print(data.shape)
+    print(files)
 

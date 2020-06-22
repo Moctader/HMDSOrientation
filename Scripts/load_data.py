e64af9c1cd9e4bca52539e7c797f50ff578d3df2
Moctader <md.moctader@student.oulu.fi>
2020-06-22 10:27:37 +0300

    modify

--- Scripts/load_data.py
+++ Scripts/load_data.py
@@ -240,7 +240,6 @@ if load_hmds:
         data_xz = np.transpose(Image8, (2, 0, 1))
         data_xz = np.flip(data_xz, axis=0)
         data_xz = np.transpose(data, (0, 2, 1))
-        data_xz = np.flip(data_xz, axis=1)
         print_orthogonal(data_xz, title=files_hmds[i])
         save(HMDS_save_test_rotated + files_hmds[i], files_hmds[i] + '_', data_xz)
        # read_image(image_png_path, image_png_file)
